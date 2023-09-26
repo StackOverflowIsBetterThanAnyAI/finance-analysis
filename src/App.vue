203024c57bf8d47146c13890d32763d0b8a8e2bd
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2023-09-26 16:24:24 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <MyInterface/>
+</template>
+
+<script>
+import MyInterface from './components/Interface.vue'
+
+export default {
+  name: 'App',
+  components: {
+        MyInterface
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#pic {
+  width: 100px;
+  height: 100px;
+  float: left;
+  margin-left: 8px;
+}
+</style>
